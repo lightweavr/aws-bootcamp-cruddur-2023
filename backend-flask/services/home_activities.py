504798d23cae971b3ee3d6e94d64787d392c5ff2
light weavr <github.lightweavr@kyl191.net>
2023-04-04 05:43:11 +0000

    fetch cruds from postgres

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 from lib.db import pool, query_wrap_array 
+# from psycopg.rows import dict_row
 
 tracer = trace.get_tracer("home.activities")
 
@@ -11,9 +12,7 @@ class HomeActivities:
             span = trace.get_current_span()
             now = datetime.now(timezone.utc).astimezone()
             span.set_attribute("app.now", now.isoformat())
-
-            sql = query_wrap_array(
-                """
+            inner_sql = """
             SELECT
                 activities.uuid,
                 users.display_name,
@@ -29,15 +28,22 @@ class HomeActivities:
             LEFT JOIN public.users ON users.uuid = activities.user_uuid
             ORDER BY activities.created_at DESC
             """
-            )
-            print(sql)
+            sql = query_wrap_array(inner_sql)
+            
             with pool.connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(sql)
-                    # this will return a tuple
-                    # the first field being the data
                     json = cur.fetchall()
-                    print(json)
+                    # print(json[0][0])
+                """
+                This next bit of code returns the similar thing as the above - except with JSON serialization 
+                issues with datetime and uuid objects. If there's a lot of mangling in the future, I can change 
+                the code that gets run
+                with conn.cursor(row_factory=dict_row) as cur:
+                    cur.execute(inner_sql)
+                    res = cur.fetchall() 
+                    print(res) 
+                """
             span.set_attribute("app.result_length", len(json))
 
-            return json[0]
+            return json[0][0]
