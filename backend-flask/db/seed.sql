c53bc64a11fd84c601cde4edd7f24b721dea8733
light weavr <github.lightweavr@kyl191.net>
2023-04-04 05:23:01 +0000

    Add postgres to backend

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,33 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO
+  public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
-
-INSERT INTO public.activities (user_uuid, message, expires_at)
+  ('Andrew Brown', 'andrewbrown', 'MOCK'),
+  ('Andrew Bayko', 'bayko', 'MOCK');
+INSERT INTO
+  public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
-    'This was imported as seed data!',
-    current_timestamp + interval '10 day'
-  )
+    (
+      SELECT
+        uuid
+      from
+        public.users
+      WHERE
+        users.handle = 'andrewbrown'
+      LIMIT
+        1
+    ), 'This was imported as seed data!', current_timestamp + interval '10 day'
+  ),
+  (
+    (
+      SELECT
+        uuid
+      from
+        public.users
+      WHERE
+        users.handle = 'bayko'
+      LIMIT
+        1
+    ), 'Another seed import!', current_timestamp + interval '10 day'
+  );
