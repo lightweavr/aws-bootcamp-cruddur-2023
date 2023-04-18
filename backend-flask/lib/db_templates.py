e29e52df6508acb45b31274c317ac147296fe0a3
light weavr <github.lightweavr@kyl191.net>
2023-04-18 05:24:27 +0000

    Add user activity fetch from db

--- backend-flask/lib/db_templates.py
+++ backend-flask/lib/db_templates.py
@@ -4,8 +4,8 @@ create = """INSERT INTO public.activities (
   expires_at
 )
 VALUES (
-  (SELECT uuid 
-    FROM public.users 
+  (SELECT uuid
+    FROM public.users
     WHERE users.handle = %(handle)s
     LIMIT 1
   ),
@@ -38,7 +38,52 @@ activities_object = """SELECT
   activities.created_at,
   activities.expires_at
 FROM public.activities
-INNER JOIN public.users ON users.uuid = activities.user_uuid 
-WHERE 
+INNER JOIN public.users ON users.uuid = activities.user_uuid
+WHERE
   activities.uuid = %(uuid)s
+"""
+
+
+get_user_activities = """SELECT
+  (SELECT COALESCE(row_to_json(object_row),'{}'::json) FROM (
+    SELECT
+      users.uuid,
+      users.cognito_user_id as cognito_user_uuid,
+      users.handle,
+      users.display_name,
+      users.bio,
+      (
+       SELECT
+        count(true)
+       FROM public.activities
+       WHERE
+        activities.user_uuid = users.uuid
+       ) as cruds_count
+  ) object_row) as profile,
+  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    SELECT
+      activities.uuid,
+      users.display_name,
+      users.handle,
+      activities.message,
+      activities.created_at,
+      activities.expires_at
+    FROM public.activities
+    WHERE
+      activities.user_uuid = users.uuid
+    ORDER BY activities.created_at DESC
+    LIMIT 40
+  ) array_row) as activities
+FROM public.users
+WHERE
+  users.handle = %(handle)s
+"""
+
+update_user_profile = """UPDATE public.users
+SET
+  bio = %(bio)s,
+  display_name= %(display_name)s
+WHERE
+  users.cognito_user_id = %(cognito_user_id)s
+RETURNING handle;
 """
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,4 +1,6 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
+from lib.db import db
+import lib.db_templates
 from aws_xray_sdk.core import xray_recorder
 
 
@@ -8,23 +10,16 @@ class UserActivities:
         # raise RuntimeError("testing the log")
         model = {"errors": None, "data": None}
         subsegment = xray_recorder.begin_subsegment("mock-data")
-        now = datetime.now(timezone.utc).astimezone()
 
         if user_handle == None or len(user_handle) < 1:
             model["errors"] = ["blank_user_handle"]
             return model
 
         now = datetime.now()
-        results = [
-            {
-                "uuid": "248959df-3079-4947-b847-9e0892d1bab4",
-                "handle": "Andrew Brown",
-                "message": "Cloud is fun!",
-                "created_at": (now - timedelta(days=1)).isoformat(),
-                "expires_at": (now + timedelta(days=31)).isoformat(),
-            }
-        ]
-        model["data"] = results
+        model["data"] = db.query_object_json(
+            lib.db_templates.get_user_activities, {"handle": user_handle}
+        )
+
         # Alternatively, just use xray_recorder directly
         subsegment.put_annotation("results_len", len(results))
         # set_user only works on the segment, not subsegement
