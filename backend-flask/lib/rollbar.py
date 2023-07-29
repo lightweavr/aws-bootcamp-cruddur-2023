115f48efe38427018bc3732c81610bf05c0ad2f0
light weavr <github.lightweavr@kyl191.net>
2023-07-29 08:13:35 +0000

    Move more logging stuff out to lib files

---
+++ backend-flask/lib/rollbar.py
@@ -0,0 +1,18 @@
+from flask import got_request_exception
+from time import strftime
+import os
+import rollbar
+import rollbar.contrib.flask
+
+def init_rollbar(app):
+    with app.app_context():
+        rollbar.init(
+            access_token=os.getenv("ROLLBAR_ACCESS_TOKEN"),
+            environment="production",
+            # server root directory, makes tracebacks prettier
+            root=os.path.dirname(os.path.realpath(__file__)),
+            # flask already sets up logging
+            allow_logging_basic_config=False,
+        )
+        rollbar.report_message("Rollbar is configured correctly", "info")
+        got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
---
+++ backend-flask/lib/xray.py
@@ -0,0 +1,10 @@
+import os
+import logging
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+def init_xray(app):
+    xray_url = os.getenv("AWS_XRAY_URL")
+    xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+    XRayMiddleware(app, xray_recorder)
+    logging.getLogger("aws_xray_sdk").setLevel(logging.DEBUG)
