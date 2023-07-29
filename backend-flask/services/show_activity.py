b41457937e763c7e54cb90f48b32307888dc5035
light weavr <github.lightweavr@kyl191.net>
2023-07-29 21:16:53 +0000

    Fix typos

--- backend-flask/services/show_activity.py
+++ backend-flask/services/show_activity.py
@@ -4,6 +4,6 @@ from lib.db import db
 import lib.db_templates
 
 
-class ShowActivities:
+class ShowActivity:
     def run(activity_uuid):
         return db.query_object_json(lib.db_templates.show, {"uuid": activity_uuid})
