ddf5dbeb8a7ba2e2bf6e6499c9a142acf7e661a1
light weavr <github.lightweavr@kyl191.net>
2023-07-29 21:17:23 +0000

    Remove old comment

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -16,21 +16,6 @@ class HomeActivities:
             span.set_attribute("app.now", now.isoformat())
             results = db.query_array_json(lib.db_templates.home, {})
 
-            # with pool.connection() as conn:
-            #     with conn.cursor() as cur:
-            #         cur.execute(sql)
-            #         json = cur.fetchall()
-            # print(json[0][0])
-            """
-            This next bit of code returns the similar thing as the above - except with JSON serialization 
-            issues with datetime and uuid objects. If there's a lot of mangling in the future, I can change 
-            the code that gets run
-            from psycopg.rows import dict_row
-            with conn.cursor(row_factory=dict_row) as cur:
-                cur.execute(inner_sql)
-                res = cur.fetchall() 
-                print(res) 
-            """
             span.set_attribute("app.result_length", len(results))
 
             return results
